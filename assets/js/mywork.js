f8f0e240d449b36b8e4abaead8c3eb23c6b6771a
Hemachandran J <103634961+hemachandran-4@users.noreply.github.com>
2022-11-15 17:54:10 +0530

    Add files via upload
    New modifications after check-1

--- assets/js/mywork.js
+++ assets/js/mywork.js
@@ -1,50 +1,52 @@
-document.querySelector("#block1work").addEventListener("mouseenter",block1Fun);
+// document.querySelector("#block1work").addEventListener("mouseenter",block1Fun);
+block1Fun();
 
-function block1Fun(event){
-    event.preventDefault();
+function block1Fun(){
     let container=document.querySelector("#kickStarterBlock");
     container.innerHTML=`<p> Crowdfunding platfrom focused on creativity and bringing them to life.</p> <h5>Tech Stack: <span class="smallSize">HTML | CSS | JS | JSON</span></h5>`;
 }
 
-document.querySelector("#block1work").addEventListener("mouseleave",block1Fun2);
+// document.querySelector("#block1work").addEventListener("mouseleave",block1Fun2);
 
-function block1Fun2(event){
-    event.preventDefault();
-    let container=document.querySelector("#kickStarterBlock");
-    container.innerHTML=null;
+// function block1Fun2(event){
+//     event.preventDefault();
+//     let container=document.querySelector("#kickStarterBlock");
+//     container.innerHTML=null;
     
-}
+// }
 
-document.querySelector("#block2work").addEventListener("mouseenter",block2Fun);
+// document.querySelector("#block2work").addEventListener("mouseenter",block2Fun);
+block2Fun();
 
-function block2Fun(event){
-    event.preventDefault();
+function block2Fun(){
     let container=document.querySelector("#asosBlock");
+    container.style.backgroundColor="white";
     container.innerHTML=`<p> ASOS is a unique online fashion shopping experience.</p> <h5>Tech Stack: <span class="smallSize">HTML | CSS | JS </span></h5>`;
 }
 
-document.querySelector("#block2work").addEventListener("mouseleave",block2Fun2);
+// document.querySelector("#block2work").addEventListener("mouseleave",block2Fun2);
 
-function block2Fun2(event){
-    event.preventDefault();
-    let container=document.querySelector("#asosBlock");
-    container.innerHTML=null;
+// function block2Fun2(event){
+//     event.preventDefault();
+//     let container=document.querySelector("#asosBlock");
+//     container.innerHTML=null;
     
-}
-document.querySelector("#block3work").addEventListener("mouseenter",block3Fun);
+// }
+// document.querySelector("#block3work").addEventListener("mouseenter",block3Fun);
+block3Fun();
 
-function block3Fun(event){
-    event.preventDefault();
+function block3Fun(){
     let container=document.querySelector("#sudokuBlock");
     container.innerHTML=`<p> A small yet expandable project, focused on data storage and optimization of stored data</p> <h5>Tech Stack: <span class="smallSize">HTML | CSS | JS | JAVA</span></h5>`;
 }
 
-document.querySelector("#block3work").addEventListener("mouseleave",block3Fun2);
+// document.querySelector("#block3work").addEventListener("mouseleave",block3Fun2);
 
-function block3Fun2(event){
-    event.preventDefault();
-    let container=document.querySelector("#sudokuBlock");
-    container.innerHTML=null;
+// function block3Fun2(event){
+//     event.preventDefault();
+//     let container=document.querySelector("#sudokuBlock");
+//     container.innerHTML=null;
     
-}
+// }
+
 
